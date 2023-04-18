6a0bb2166ecbd2f2631e1b2e62e8fa717ce13c37
Vivien Lee <vivienlvv@gmail.com>
2023-04-17 19:41:29 -0500

    Updated web scraping portion

--- R/week12.R
+++ R/week12.R
@@ -1,27 +1,32 @@
 # Script for easy testing
+library(tidyverse)
 library(httr)
+library(rvest)
+library(tictoc)
 
-## Modified function from class example 
-counter = 0 # try scraping 2 pages to start
-responses_tbl = tibble() # creating tibble to store responses
-get_url = "https://old.reddit.com/r/IOPsychology/" # initial start url
+responses_tbl = tibble() # Creating tibble to store response from get request
+get_url = "https://old.reddit.com/r/IOPsychology/" # Initial get url
+counter = 1 
 
-while(counter < 3){
-  response = (content(GET(get_url, user_agent("UMN Student lee02903@umn.edu")), as = "text"))
-  responses_tbl = bind_rows(responses_tbl, tibble(response))
-  print(paste("Just received data from", get_url))
+tic()
+## This is a while loop goes through all visible pages in the io subreddit 
+### I initially set it to 100 pages but ran into an error at page 40 because 
+### there are only 40 pages right now, so I set 40 for the while loop 
+
+while(counter <= 40){
+  response = content(GET(get_url, user_agent("UMN Student lee02903@umn.edu")), as = "text")
+  responses_tbl = bind_rows(responses_tbl, tibble(response)) # storing get response to tibble
+  print(paste(counter, "Just received data from", get_url))
   
   next_url = read_html(response) %>% html_elements("span.next-button a") %>% html_attr("href")
-  get_url = next_url
+  get_url = next_url # Setting url for next iteration
+  counter = counter + 1 # Incrementing counter for while loop
   
-  counter = counter + 1
-  Sys.sleep(2)
+  Sys.sleep(2) # Pause to conform to rate limit
 }
+toc()
 
-
-page_ls = responses_tbl$response
-
-# creating a function that gets the post title and num of upvotes
+## Creating a function that gets post title and num of upvotes for each page
 get_info = function(single_page){
   single_page = read_html(single_page)
   xpath_post = "//div[contains(@class, 'odd') or contains(@class, 'even')]//a[contains(@class, 'title may-blank')]"
@@ -31,13 +36,19 @@ get_info = function(single_page){
   upvotes = html_elements(single_page, xpath = xpath_upvotes) %>% 
     html_text() %>% 
     as.numeric() 
-  io_tbl = tibble(post, upvotes)
+  
+  # Added for myself  to see the post date
+  time =  html_elements(single_page, "time:nth-child(1)") %>% html_text() 
+  
+  io_tbl = tibble(time = time[-1], post, upvotes)
   return(io_tbl)
 }
 
-# this is a list containing separate tibbles for each page 
-result = lapply(responses_tbl$response, get_info)
+## Outputting a list of posts containing a separate tibble for each page 
+posts_ls = lapply(responses_tbl$response, get_info)
 
-# to collapse all the list to form one giant dataset
-week12_tbl = result %>% bind_rows()
+## Collapsing all tibbles in the list to form one giant tibble
+week12_tbl =  bind_rows(posts_ls) %>% select(-time)
 
+## Saving scraped dataset as week12_tbl.csv
+write_csv(week12_tbl, "../data/week12_tbl.csv")
